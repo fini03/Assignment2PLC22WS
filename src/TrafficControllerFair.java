0ade6ad57d29e43606d4b534e527fd90bf6d46c7
Finisher <112564342+fini03@users.noreply.github.com>
2023-02-13 01:12:05 +0100

    initial commit

---
+++ src/TrafficControllerFair.java
@@ -0,0 +1,59 @@
+/**
+ * @author Lamies Abbas
+ * @id 12128050
+ */
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TrafficControllerFair implements TrafficController {
+    private final TrafficRegistrar registrar;
+    Lock lock = new ReentrantLock(true);
+    private final Condition place_empty = lock.newCondition();
+    private boolean empty = true;
+
+    public TrafficControllerFair(TrafficRegistrar registrar) {
+        this.registrar = registrar;
+    }
+
+    @Override
+    public void enterLeft(Vehicle v) {
+        lock.lock();
+        try {
+            while(!empty) {
+                place_empty.await();
+            }
+            registrar.registerLeft(v);
+            empty = false;
+        } catch (InterruptedException e) {}
+    }
+
+    @Override
+    public void enterRight(Vehicle v) {
+        lock.lock();
+        try {
+            while(!empty) {
+                place_empty.await();
+            }
+            registrar.registerRight(v);
+            empty = false;
+        } catch (InterruptedException e) {}
+    }
+
+    @Override
+    public void leaveLeft(Vehicle v) {
+        empty = true;
+        registrar.deregisterLeft(v);
+        place_empty.signalAll();
+        lock.unlock();
+    }
+
+    @Override
+    public void leaveRight(Vehicle v) {
+        empty = true;
+        registrar.deregisterRight(v);
+        place_empty.signalAll();
+        lock.unlock();
+    }
+}
---
+++ src/TrafficControllerSimple.java
@@ -0,0 +1,54 @@
+/**
+ * @author Lamies Abbas
+ * @id 12128050
+ */
+
+public class TrafficControllerSimple implements TrafficController {
+    private final TrafficRegistrar registrar;
+    private boolean empty = true;
+    
+    public TrafficControllerSimple(TrafficRegistrar registrar) {
+        this.registrar = registrar;
+    }
+
+    private void enter() {
+        while(!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        empty = true;
+        notifyAll();
+    }
+
+    private void leave() {
+        empty = true;
+        notifyAll();
+    }
+
+    @Override
+    public synchronized void enterLeft(Vehicle v) {
+        enter();
+        registrar.registerLeft(v);
+    }
+
+    @Override
+    public synchronized void enterRight(Vehicle v) {
+        enter();
+        registrar.registerRight(v);
+    }
+
+    @Override
+    public synchronized void leaveLeft(Vehicle v) {
+        leave();
+        registrar.deregisterLeft(v);
+    }
+
+    @Override
+    public synchronized void leaveRight(Vehicle v) {
+        leave();
+        registrar.deregisterRight(v);
+    }
+}
